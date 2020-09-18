c7974387c9bc64735d066cc2065e533130a7be5e
JuliaPanPan <55499865+JuliaPanPan@users.noreply.github.com>
2020-09-18 13:44:31 -0400

    Create Drills.java
    drills for studying for unit 1 test

---
+++ src/Drills.java
@@ -0,0 +1,54 @@
+public class Drills{
+	
+	public static void main(String[] args){
+	
+		// Declare 5 different data types with initial values
+        /*int x = 5;
+        String num = "Hello";
+        double y = 1.2;
+        char set = 'x';
+        boolean julia = true; 
+		
+		for(int i=0; i<3; i++) {
+            System.out.print(" I am having a good day");
+        }
+		
+        String[] food = {"pizza ", "hamburger ","fries ", "potatoes "};
+        for(String Julia: food) {
+            System.out.print(Julia);
+        }
+        
+        
+        // An infinite loop
+        
+        while (true){
+            if (1==0 && 5 + 6 == 10) {
+                System.out.println("This will not print");
+            }
+            break;
+        }*/
+
+        
+			// a short-circut conditional with four tests
+		
+				// Break a loop if a conditional passes
+		
+        // Loop through each char in a String
+    
+            for (int i=0; i<one.length(); i++) {
+                 System.out.print("Hello!");
+            }
+				
+		// Print only the first three letters in “word”
+		
+		// Print all the odd numbers from 1 - 100
+		
+		// Create a Scanner and take an input
+		
+		// Create a Scanner, take a number, and count down from that number to 0
+		
+	}
+	
+	// Create a method that returns a comparison (include a JavaDoc comment)
+	
+}
